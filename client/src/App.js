85b3da2c289ec42a43143c8d76e8293386c4e2e4
Carson Meiling <carmc93@gmail.com>
2020-03-25 19:38:15 -0600

    navbar and navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Home from './components/shared/Home';
+import About from './components/shared/About';
+import NoMatch from './components/shared/NoMatch';
+import Navbar from './components/shared/Navbar';
+import { Container } from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => (
+  <>
+  <Navbar />
+  <Container>
+  <Switch>
+    <Route exact path='/' component={Home} />
+    <Route exact path='/about' component={About} />
+    <Route component={NoMatch} />
+  </Switch>
+  </Container>
+  </>
+)
 
 export default App;
---
+++ client/src/components/shared/NoMatch.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+const NoMatch = () => (
+  <>
+  <h1>404 page not found</h1>
+  <Link to='/'>
+    Return to home page
+  </Link>
+  </>
+)
+
+export default NoMatch;
